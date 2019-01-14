188b290b5be75e047ed3f615478eaa6869653d07
Delagardi <delagardi@ymail.com>
2019-01-14 05:29:52 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 const { dispatch } = store;
 
-
-
-const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
-  incDispatch: inc,
-  decDispatch: dec,
-  rndDispatch: rnd
-}, dispatch);
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 document
   .getElementById('inc')
-  .addEventListener('click', incDispatch);
+  .addEventListener('click', inc);
 
 document
   .getElementById('dec')
-  .addEventListener('click', decDispatch);
+  .addEventListener('click', dec);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random()*10);
-    rndDispatch(payload);
+    rnd(payload);
   });
 
 const update = () => {
